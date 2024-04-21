fadc8d5255b0e335e1e08d8688381f9c1f9d0895
saenokami <165577235+saenokami@users.noreply.github.com>
2024-04-21 14:01:19 +0000

    [Add]コメント投稿機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@
   devise_for :users
   root to: 'homes#top'
   get "/homes/about" => "homes#about", as: "about"
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+   resources :post_comments, only: [:create]
+  end
   resources :users, only: [:show, :edit, :update]
   
 end
